2c85dfc3c80431366e87712d5f076baed07794dd
youwuyou <youwuyou@student.ethz.ch>
2023-09-29 18:26:31 -0400

    not tracking the cmake-build-debug folder

--- cmake-build-debug/googletest/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youwuyou/workspace/HS23/SoftwareEngineering/cse-lama-example-project/googletest/lib
-# Build directory: /home/youwuyou/workspace/HS23/SoftwareEngineering/cse-lama-example-project/cmake-build-debug/googletest/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youwuyou/workspace/HS23/SoftwareEngineering/cse-lama-example-project/googletest/lib/googlemock
-# Build directory: /home/youwuyou/workspace/HS23/SoftwareEngineering/cse-lama-example-project/cmake-build-debug/googletest/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
